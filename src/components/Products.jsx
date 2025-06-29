4134b1abd3e97bbd181fdc550238eaee3e851097
Adharsh0 <118553385+Adharsh0@users.noreply.github.com>
2025-06-29 23:01:04 +0530

    final

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -214,11 +214,7 @@ const Products = () => {
                   />
                 </div>
                 
-                <div className="quality-badge1">
-                  <svg className="badge-icon1" fill="none" stroke="currentColor" strokeWidth="2.5" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
-                </div>
+                
 
                 <div className="image-glow1"></div>
               </div>
